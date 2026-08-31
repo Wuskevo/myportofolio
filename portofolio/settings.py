43b9c2b9aba456a8f0b5b654b99e5d6f08a37b45
Wskv <116175181+Wuskevo@users.noreply.github.com>
2026-08-31 18:07:02 +0700

    feat: add landing page view and update URL routing

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'portofolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,6 +75,9 @@ TEMPLATES = [
         },
     },
 ]
+
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 WSGI_APPLICATION = 'portofolio.wsgi.application'
 
