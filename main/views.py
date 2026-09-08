d083dd7723e8c0a43efd0a93caf3cc475accd8d6
Wskv <116175181+Wuskevo@users.noreply.github.com>
2026-09-08 21:02:11 +0700

    feat: hands-on showing mahasiswa

--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from main.models import Experience
+from main.models import Experience, Mahasiswa
 
 
 def show_main(request):
@@ -22,3 +22,10 @@ def show_experience(request):
         "experience_list": Experience.objects.all(),
     }
     return render(request, "experience.html", context)
+
+def show_mahasiswa(request):
+    context = {
+        "name": "Clement Kevin Tanadi",
+        "mahasiswa_list": Mahasiswa.objects.all(),
+    }
+    return render(request, "mahasiswa.html", context)
