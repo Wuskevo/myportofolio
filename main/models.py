6315e25e0b13ac07a8153a028b797ce1b53510d8
Wskv <116175181+Wuskevo@users.noreply.github.com>
2026-09-10 21:32:40 +0700

    feat: add credential model
    - Added Credential model to represent certifications and achievements.
    - Added fields for title, category, issuer, date earned, expiry date, verification URL, description, and image.
    - Added credential images container dir media/credentials/.
    - Configured optional expiry dates, verification URLs, and images.
    - Installed Pillow to support ImageField.

--- main/models.py
+++ main/models.py
@@ -31,3 +31,46 @@ class Experience(models.Model):
     @property
     def is_ongoing(self):
         return self.ended_at is None
+
+"""
+Added this instead of achievements and certifications because there is a lot of overlap currently
+Instead of complicated inheritance, enums are used, following the pattern from [Experience]
+"""
+class Credential(models.Model):
+    CREDENTIAL_CATEGORIES = [
+        ("certification", "Certification"),
+        ("award", "Award"),
+        ("competition", "Competition"),
+        ("scholarship", "Scholarship")
+    ]
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    category = models.CharField(
+        max_length=20,
+        choices=CREDENTIAL_CATEGORIES,
+        default="full-time",
+    )
+
+    ## organization or event
+    issuer = models.CharField(max_length=255)
+
+    ## date of earning this credential
+    date_received = models.DateField()
+
+    ## date of expiry, may not apply
+    expiry_date = models.DateField(blank=True, null=True)
+
+    ## link/url to some source to verify this credential
+    credential_url = models.URLField(blank=True)
+
+    ## image relevant to this credential like trophy, certificate, badge, etc
+    image = models.ImageField(
+        upload_to="credentials/",
+        blank=True,
+        null=True
+    )
+    
+
+        
