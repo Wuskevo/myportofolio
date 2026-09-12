a5fd29246921aca8754b3b249c12b7c3b6990bca
Wskv <116175181+Wuskevo@users.noreply.github.com>
2026-09-12 23:06:50 +0700

    chore: credential views and urls wiring

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from main.views import show_main, show_experience
+from main.views import show_main, show_experience, show_credential
 
 app_name = "main"
 
 urlpatterns = [
     path("", show_main, name="show_main"),
     path("experience/", show_experience, name="show_experience"),
+    path("credential/", show_credential, name="show_credential"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from main.models import Experience
+from main.models import Experience, Credential
 
 
 def show_main(request):
@@ -22,3 +22,11 @@ def show_experience(request):
         "experience_list": Experience.objects.all(),
     }
     return render(request, "experience.html", context)
+
+
+def show_credential(request):
+    context = {
+        "name": "Clement Kevin Tanadi",
+        "credential_list": Credential.objects.all(),
+    }
+    return render(request, "credential.html", context)
